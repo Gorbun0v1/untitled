a827569069a72a5b1174880505d502e8e9531caf
Gorbun0v1 <joongleboo01@gmail.com>
2023-12-10 03:23:57 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
+            // Строка для разделения
+            String st1 = " Напишите код, с помощью которого можно разделить строку на части. ";
+            // Разделение строки по пробелу через foreach
+            String[] st2 = st1.split(" ");
+            // Цикл foreach
+            System.out.println("Разделение строки: ");
+            for(String st3: st2) {
+                System.out.println(st3);
+            }
 
     }
 }
